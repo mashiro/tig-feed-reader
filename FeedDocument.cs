8260d6fd10a270c02301a90b7326da28cf40eda0
mashiro <y.mashiro@gmail.com>
2009-12-07 15:08:40 +0900

    first commit

---
+++ FeedDocument.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Spica.Xml.Feed.Simple
+{
+	/// <summary>
+	/// Utility
+	/// </summary>
+	public class Utility
+	{
+		public static DateTime ParseDateTime(String dateTimeString)
+		{
+			DateTime dateTime;
+			if (!DateTime.TryParse(dateTimeString, out dateTime))
+				dateTime = DateTime.Now;
+
+			return dateTime;
+		}
+
+		public static Uri ParseUri(String uriString)
+		{
+			if (String.IsNullOrEmpty(uriString))
+				return null;
+
+			return new Uri(uriString);
+		}
+
+		public static TResult ValueOrDefault<T, TResult>(T arg, Func<T, TResult> func)
+			where T : class
+		{
+			if (arg == null)
+				return default(TResult);
+
+			return func(arg);
+		}
+	}
+
+	/// <summary>
+	/// Feed Document Interface
+	/// </summary>
+	public interface IFeedDocument
+	{
+		Uri Link { get; }
+		String Title { get; }
+		String Description { get; }
+		List<IFeedItem> Items { get; }
+	}
+
+	/// <summary>
+	/// Feed Item Interface
+	/// </summary>
+	public interface IFeedItem
+	{
+		String Author { get; }
+		Uri Link { get; }
+		String Title { get; }
+		String Description { get; }
+		DateTime PublishDate { get; }
+	}
+
+	#region RSS1.0
+	[XmlRoot("RDF", Namespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#")]
+	public class RSS10Document : IFeedDocument
+	{
+		#region XmlSerializer
+		private static Object _sync = new object();
+		private static XmlSerializer _serializer = null;
+		static RSS10Document()
+		{
+			lock (_sync)
+			{
+				if (_serializer == null)
+					_serializer = new XmlSerializer(typeof(RSS10Document));
+			}
+		}
+		public static XmlSerializer Serializer { get { return _serializer; } }
+		#endregion
+
+		[XmlElement("channel", Namespace = "http://purl.org/rss/1.0/")]
+		public RSS10Channel _channel;
+
+		[XmlElement("item", Namespace = "http://purl.org/rss/1.0/")]
+		public List<RSS10Item> _items;
+
+		#region IFeedDocument メンバ
+		[XmlIgnore()]
+		public Uri Link { get { return _channel.Link; } }
+
+		[XmlIgnore()]
+		public string Title { get { return _channel.Title; } }
+
+		[XmlIgnore()]
+		public string Description { get { return _channel.Description; } }
+
+		[XmlIgnore()]
+		public List<IFeedItem> Items { get { return new List<IFeedItem>(_items.ToArray()); } }
+		#endregion
+	}
+
+	public class RSS10Channel
+	{
+		#region XmlElements
+		[XmlElement("title")]
+		public String _title;
+
+		[XmlElement("link")]
+		public String _link;
+
+		[XmlElement("description")]
+		public String _description;
+
+		[XmlElement("date", Namespace = "http://purl.org/dc/elements/1.1/")]
+		public String _date;
+
+		[XmlElement("language", Namespace = "http://purl.org/dc/elements/1.1/")]
+		public String _language;
+		#endregion
+
+		[XmlIgnore()]
+		public String Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return Utility.ParseUri(_link); } }
+
+		[XmlIgnore()]
+		public String Description { get { return _description; } }
+
+		[XmlIgnore()]
+		public DateTime PublishDate { get { return Utility.ParseDateTime(_date); } }
+	}
+
+	public class RSS10Item : IFeedItem
+	{
+		[XmlElement("title")]
+		public String _title;
+
+		[XmlElement("link")]
+		public String _link;
+
+		[XmlElement("description")]
+		public String _description;
+
+		[XmlElement("creator", Namespace = "http://purl.org/dc/elements/1.1/")]
+		public String _creator;
+
+		[XmlElement("date", Namespace = "http://purl.org/dc/elements/1.1/")]
+		public String _date;
+
+		#region IFeedItem メンバ
+		[XmlIgnore()]
+		public string Author { get { return _creator; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return Utility.ParseUri(_link); } }
+
+		[XmlIgnore()]
+		public string Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public string Description { get { return _description; } }
+
+		[XmlIgnore()]
+		public DateTime PublishDate { get { return Utility.ParseDateTime(_date); } }
+		#endregion
+	}
+	#endregion
+
+	#region RSS2.0
+	[XmlRoot("rss")]
+	public class RSS20Document : IFeedDocument
+	{
+		#region XmlElements
+		[XmlElement("channel")]
+		public RSS20Channel _channel;
+		#endregion
+
+		#region Interface
+		[XmlIgnore()]
+		public String Title { get { return _channel._title; } }
+
+		[XmlIgnore()]
+		public String Description { get { return _channel._description; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return _channel.Link; } }
+
+		[XmlIgnore()]
+		public List<IFeedItem> Items { get { return new List<IFeedItem>(_channel._items.ToArray()); } }
+		#endregion
+
+		#region XmlSerializer
+		private static Object _sync = new object();
+		private static XmlSerializer _serializer = null;
+		static RSS20Document()
+		{
+			lock (_sync)
+			{
+				if (_serializer == null)
+					_serializer = new XmlSerializer(typeof(RSS20Document));
+			}
+		}
+		public static XmlSerializer Serializer { get { return _serializer; } }
+		#endregion
+	}
+
+	public class RSS20Channel
+	{
+		#region XmlElements
+		[XmlElement("title")]
+		public String _title;
+
+		[XmlElement("description")]
+		public String _description;
+
+		[XmlElement("link")]
+		public String _link;
+
+		[XmlElement("pubDate")]
+		public String _pubDate;
+
+		[XmlElement("item")]
+		public List<RSS20Item> _items;
+		#endregion
+
+		[XmlIgnore()]
+		public String Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public String Description { get { return _description; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return Utility.ParseUri(_link); } }
+
+		[XmlIgnore()]
+		public DateTime PublishDate { get { return Utility.ParseDateTime(_pubDate); } }
+	}
+
+	public class RSS20Item : IFeedItem
+	{
+		#region XmlElements
+		[XmlElement("author")]
+		public String _author;
+
+		[XmlElement("link")]
+		public String _link;
+
+		[XmlElement("pubDate")]
+		public String _pubDate;
+
+		[XmlElement("title")]
+		public String _title;
+
+		[XmlElement("description")]
+		public String _description;
+		#endregion
+
+		#region Interface
+		[XmlIgnore()]
+		public String Author { get { return _author; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return Utility.ParseUri(_link); } }
+
+		[XmlIgnore()]
+		public String Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public String Description { get { return _description; } }
+
+		[XmlIgnore()]
+		public DateTime PublishDate { get { return Utility.ParseDateTime(_pubDate); } }
+		#endregion
+	}
+	#endregion
+
+	#region Atom1.0
+	[XmlRoot("feed", Namespace = "http://www.w3.org/2005/Atom")]
+	public class Atom10Document : IFeedDocument
+	{
+		#region XmlElements
+		[XmlElement("title")]
+		public String _title;
+
+		[XmlElement("subtitle")]
+		public String _subtitle;
+
+		[XmlElement("link")]
+		public List<Atom10Link> _link;
+
+		[XmlElement("entry")]
+		public List<Atom10Item> _items;
+		#endregion
+
+		#region Interface
+		[XmlIgnore()]
+		public String Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public String Description { get { return _subtitle; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return _link.Count > 0 ? _link[0].HRef : null; } }
+
+		[XmlIgnore()]
+		public List<IFeedItem> Items { get { return new List<IFeedItem>(_items.ToArray()); } }
+		#endregion
+
+		#region XmlSerializer
+		private static Object _sync = new object();
+		private static XmlSerializer _serializer = null;
+		static Atom10Document()
+		{
+			lock (_sync)
+			{
+				if (_serializer == null)
+					_serializer = new XmlSerializer(typeof(Atom10Document));
+			}
+		}
+		public static XmlSerializer Serializer { get { return _serializer; } }
+		#endregion
+	}
+
+	public class Atom10Item : IFeedItem
+	{
+		#region XmlElements
+		[XmlElement("author")]
+		public Atom10Person _author;
+
+		[XmlElement("link")]
+		public List<Atom10Link> _link;
+
+		[XmlElement("title")]
+		public String _title;
+
+		[XmlElement("content")]
+		public String _content;
+
+		[XmlElement("published")]
+		public String _published;
+		#endregion
+
+		#region Interface
+		[XmlIgnore()]
+		public String Author { get { return _author.Name; } }
+
+		[XmlIgnore()]
+		public Uri Link { get { return _link.Count > 0 ? _link[0].HRef : null; } }
+
+		[XmlIgnore()]
+		public String Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public String Description { get { return _content; } }
+
+		[XmlIgnore()]
+		public DateTime PublishDate { get { return DateTime.Parse(_published); } }
+		#endregion
+	}
+
+	public class Atom10Person
+	{
+		#region XmlElements
+		[XmlElement("name")]
+		public String _name;
+
+		[XmlElement("url")]
+		public String _uri;
+
+		[XmlElement("email")]
+		public String _email;
+		#endregion
+
+		[XmlIgnore()]
+		public String Name { get { return _name; } }
+
+		[XmlIgnore()]
+		public Uri Uri { get { return Utility.ParseUri(_uri); } }
+
+		[XmlIgnore()]
+		public String Email { get { return _email; } }
+	}
+
+	public class Atom10Link
+	{
+		#region XmlElements
+		[XmlAttribute("href")]
+		public String _href;
+
+		[XmlAttribute("rel")]
+		public String _rel;
+
+		[XmlAttribute("type")]
+		public String _type;
+
+		[XmlAttribute("hreflang")]
+		public String _hreflang;
+
+		[XmlAttribute("title")]
+		public String _title;
+
+		[XmlAttribute("length")]
+		public String _length;
+		#endregion
+
+		[XmlIgnore()]
+		public Uri HRef { get { return Utility.ParseUri(_href); } }
+
+		[XmlIgnore()]
+		public String Rel { get { return _rel; } }
+
+		[XmlIgnore()]
+		public String Type { get { return _type; } }
+
+		[XmlIgnore()]
+		public String HrefLang { get { return _hreflang; } }
+
+		[XmlIgnore()]
+		public String Title { get { return _title; } }
+
+		[XmlIgnore()]
+		public String Length { get { return _length; } }
+	}
+	#endregion
+
+	#region Feed Document
+	public static class FeedDocument
+	{
+		public static IFeedDocument Load(String url)
+		{
+			using (XmlReader reader = XmlReader.Create(url))
+			{
+				return Load(reader);
+			}
+		}
+
+		public static IFeedDocument Load(XmlReader reader)
+		{
+			if (RSS20Document.Serializer.CanDeserialize(reader))
+			{
+				return RSS20Document.Serializer.Deserialize(reader) as RSS20Document;
+			}
+			else if (RSS10Document.Serializer.CanDeserialize(reader))
+			{
+				return RSS10Document.Serializer.Deserialize(reader) as RSS10Document;
+			}
+			else if (Atom10Document.Serializer.CanDeserialize(reader))
+			{
+				return Atom10Document.Serializer.Deserialize(reader) as Atom10Document;
+			}
+
+			throw new Exception("Unknown feed format.");
+		}
+	}
+	#endregion
+}
---
+++ FeedReader.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+using Misuzilla.Net.Irc;
+using Misuzilla.Applications.TwitterIrcGateway;
+using Misuzilla.Applications.TwitterIrcGateway.AddIns;
+using Misuzilla.Applications.TwitterIrcGateway.AddIns.Console;
+using Misuzilla.Applications.TwitterIrcGateway.AddIns.TypableMap;
+using Spica.Xml.Feed.Simple;
+
+namespace Spica.Applications.TwitterIrcGateway.AddIns.FeedReader
+{
+	public class FeedReceiveEventArgs : EventArgs
+	{
+		public IFeedDocument Document { get; private set; }
+		public IFeedItem Item { get; private set; }
+		public FeedReceiveEventArgs(IFeedDocument doc, IFeedItem item)
+		{
+			Document = doc;
+			Item = item;
+		}
+	}
+
+	public class FeedReaderUrlConfiguration : IConfiguration, IDisposable
+	{
+		[Description("フィードの URL を指定します")]
+		public String Url { get; set; }
+
+		[Description("チェックする間隔を秒単位で指定します")]
+		public Int32 Interval { get; set; }
+
+		[Description(@"
+コンテンツの形式を指定します
+  #{feed_title} - フィードのタイトル
+  #{feed_link} - フィードのリンク
+  #{feed_description} - フィードの説明
+  #{author} - 記事の著者
+  #{link} - 記事のリンク
+  #{title} - 記事のタイトル
+  #{description} - 記事の説明
+  #{publish_date} - 記事の公開された日時")]
+		public String ContentFormat { get; set; }
+
+		[Description("コンテンツが流れるチャンネル名を指定します")]
+		public String ChannelName { get; set; }
+
+		[Description("コンテンツを送るユーザ名を指定します")]
+		public String SenderNick { get; set; }
+
+		[Description("フィードを有効化または無効化します")]
+		public Boolean Enabled { get; set; }
+
+		[Description("HTML タグの除去を有効化または無効化します")]
+		public Boolean EnableRemoveHtmlTag { get; set; }
+
+		[Description("改行コードの除去を有効化または無効化します")]
+		public Boolean EnableRemoveLineBreak { get; set; }
+
+		[Browsable(false)]
+		public DateTime LastPublishDate { get; set; }
+
+		public event EventHandler<ErrorEventArgs> ErrorHandled;
+		public event EventHandler PublishDateUpdated;
+		public event EventHandler<FeedReceiveEventArgs> FeedItemReceived;
+		private Timer _timer = null;
+
+		public FeedReaderUrlConfiguration()
+		{
+			Enabled = true;
+			Url = String.Empty;
+			ContentFormat = "#{title} #{link}";
+			ChannelName = "#FeedReader";
+			SenderNick = "FeedReader";
+			Interval = 60 * 60;
+			EnableRemoveHtmlTag = false;
+			EnableRemoveLineBreak = false;
+		}
+
+		#region Crawl
+		public void UpdateCrawlState()
+		{
+			if (Enabled && !String.IsNullOrEmpty(Url))
+				StartCrawl();
+			else
+				EndCrawl();
+		}
+
+		public void StartCrawl()
+		{
+			if (_timer == null)
+				_timer = new Timer(OnTimerCallback, null, Timeout.Infinite, Timeout.Infinite);
+
+			Int32 intervalMillSec = Interval * 1000;
+			_timer.Change(intervalMillSec, intervalMillSec);
+		}
+
+		public void EndCrawl()
+		{
+			if (_timer != null)
+			{
+				_timer.Dispose();
+				_timer = null;
+			}
+		}
+
+		public void CrawlForce()
+		{
+			ThreadPool.QueueUserWorkItem((state) => { OnTimerCallback(state); });
+		}
+
+		private void OnTimerCallback(Object state)
+		{
+			try
+			{
+				IFeedDocument feed = FeedDocument.Load(Url);
+
+				var updates = feed.Items.Where(item => item.PublishDate > LastPublishDate).ToList();
+				if (updates.Count > 0)
+				{
+					LastPublishDate = updates.Max(item => item.PublishDate);
+					if (PublishDateUpdated != null)
+						PublishDateUpdated(this, EventArgs.Empty);
+
+					updates.Sort((a, b) => Comparer<DateTime>.Default.Compare(a.PublishDate, b.PublishDate));
+					foreach (var item in updates)
+					{
+						if (FeedItemReceived != null)
+							FeedItemReceived(this, new FeedReceiveEventArgs(feed, item));
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				if (ErrorHandled != null)
+					ErrorHandled(this, new ErrorEventArgs(ex));
+			}
+		}
+		#endregion
+
+		public void Dispose()
+		{
+			EndCrawl();
+		}
+
+		public override string ToString()
+		{
+			return ToShortString();
+		}
+
+		public string ToShortString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat("{0} ({1})", Url, Interval);
+			return sb.ToString();
+		}
+
+		public string ToLongString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat("[{0}]", Enabled ? "*" : " ");
+			sb.AppendFormat(" {0} ({1})", Url, Interval);
+			return sb.ToString();
+		}
+	}
+
+	public class FeedReaderConfiguration : IConfiguration, IDisposable
+	{
+		[Browsable(false)]
+		public List<FeedReaderUrlConfiguration> Items { get; set; }
+
+		public FeedReaderConfiguration()
+		{
+			Items = new List<FeedReaderUrlConfiguration>();
+		}
+
+		[Browsable(false)]
+		public void Dispose()
+		{
+			foreach (var item in Items)
+			{
+				item.Dispose();
+			}
+		}
+
+		[Browsable(false)]
+		public void Setup(FeedReaderAddIn addIn)
+		{
+			foreach (var item in Items)
+			{
+				addIn.RegisterEvent(item);
+				item.UpdateCrawlState();
+			}
+		}
+	}
+
+	[Description("フィードの設定を行うコンテキストに切り替えます")]
+	public class FeedReaderContext : Context
+	{
+		private FeedReaderAddIn AddIn { get { return CurrentSession.AddInManager.GetAddIn<FeedReaderAddIn>(); } }
+
+		public override IConfiguration[] Configurations { get { return new IConfiguration[] { AddIn.Config }; } }
+		protected override void OnConfigurationChanged(IConfiguration config, System.Reflection.MemberInfo memberInfo, object value)
+		{
+			if (config is FeedReaderConfiguration)
+			{
+				AddIn.Config = config as FeedReaderConfiguration;
+				AddIn.SaveConfig();
+			}
+		}
+
+		[Description("指定したフィードを強制的に更新します")]
+		public void UpdateForce(String arg)
+		{
+			FindAt(arg, item =>
+			{
+				item.CrawlForce();
+				Console.NotifyMessage(String.Format("フィード {0} を更新しました。", item));
+			});
+		}
+
+		[Description("存在するフィードをすべて表示します")]
+		public void List()
+		{
+			if (AddIn.Config.Items.Count == 0)
+			{
+				Console.NotifyMessage("フィードは現在設定されていません。");
+				return;
+			}
+
+			for (Int32 i = 0; i < AddIn.Config.Items.Count; ++i)
+			{
+				var item = AddIn.Config.Items[i];
+				Console.NotifyMessage(String.Format("{0}: {1}", i, item.ToLongString()));
+			}
+		}
+
+		[Description("指定したフィードを有効化します")]
+		public void Enable(String arg)
+		{
+			SwitchEnable(arg, true);
+		}
+
+		[Description("指定したフィードを無効化します")]
+		public void Disable(String arg)
+		{
+			SwitchEnable(arg, false);
+		}
+
+		[Description("指定したフィードを削除します")]
+		public void Remove(String arg)
+		{
+			FindAt(arg, item =>
+			{
+				item.Dispose();
+				AddIn.Config.Items.Remove(item);
+				AddIn.SaveConfig();
+				Console.NotifyMessage(String.Format("フィード {0} を削除しました。", item));
+			});
+		}
+
+		[Description("指定したフィードを編集します")]
+		public void Edit(String arg)
+		{
+			FindAt(arg, item =>
+			{
+				var context = Console.GetContext<FeedReaderEditContext>(CurrentServer, CurrentSession) as FeedReaderEditContext;
+				context.Item = item;
+				context.IsNew = false;
+				Console.PushContext(context);
+			});
+		}
+
+		[Description("フィードを新規追加します")]
+		public void New()
+		{
+			FeedReaderEditContext context = Console.GetContext<FeedReaderEditContext>(CurrentServer, CurrentSession) as FeedReaderEditContext;
+
+			var item = new FeedReaderUrlConfiguration();
+			AddIn.RegisterEvent(item);
+
+			context.Item = item;
+			context.IsNew = true;
+			Console.PushContext(context);
+		}
+
+		private void SwitchEnable(String arg, Boolean enable)
+		{
+			FindAt(arg, item =>
+			{
+				item.Enabled = enable;
+				AddIn.SaveConfig();
+				Console.NotifyMessage(String.Format("フィード {0} を{1}化しました。", item, (enable ? "有効" : "無効")));
+
+				item.UpdateCrawlState();
+			});
+		}
+
+		private void FindAt(String arg, Action<FeedReaderUrlConfiguration> action)
+		{
+			Int32 index;
+			if (Int32.TryParse(arg, out index))
+			{
+				if (index < AddIn.Config.Items.Count && index > -1)
+				{
+					action(AddIn.Config.Items[index]);
+				}
+				else
+				{
+					Console.NotifyMessage("存在しないフィードが指定されました。");
+				}
+			}
+			else
+			{
+				Console.NotifyMessage("設定のフィードが正しくありません。");
+			}
+		}
+	}
+
+	public class FeedReaderEditContext : Context
+	{
+		private FeedReaderAddIn AddIn { get { return CurrentSession.AddInManager.GetAddIn<FeedReaderAddIn>(); } }
+		public FeedReaderUrlConfiguration Item { get; set; }
+		public Boolean IsNew { get; set; }
+		private Boolean _urlChanged = false;
+
+		public override IConfiguration[] Configurations { get { return new IConfiguration[] { Item }; } }
+		public override string ContextName { get { return (IsNew ? "New" : "Edit") + typeof(FeedReaderUrlConfiguration).Name; } }
+
+		protected override void OnConfigurationChanged(IConfiguration config, System.Reflection.MemberInfo memberInfo, object value)
+		{
+			if (config is FeedReaderUrlConfiguration)
+			{
+				if (memberInfo.Name == "Url")
+				{
+					_urlChanged = true;
+				}
+			}
+		}
+
+		[Description("フィードを保存してコンテキストを終了します")]
+		public void Save()
+		{
+			// 状態を保存
+			if (_urlChanged) Item.LastPublishDate = DateTime.MinValue;
+			if (IsNew) AddIn.Config.Items.Add(Item);
+			AddIn.SaveConfig();
+
+#if false
+			// チャンネルに JOIN する
+			if (!CurrentSession.Groups.ContainsKey(Item.ChannelName))
+			{
+				CurrentSession.SendServer(new JoinMessage(Item.ChannelName, ""));
+				CurrentSession.SendServer(new ModeMessage(Item.ChannelName, "+s"));
+				CurrentSession.SendServer(new ModeMessage(Item.ChannelName, "+n"));
+				CurrentSession.SendServer(new ModeMessage(Item.ChannelName, "+i"));
+			}
+#endif
+
+			// クローラの状態を更新
+			Item.UpdateCrawlState();
+
+			Console.NotifyMessage(String.Format("フィードを{0}しました。", (IsNew ? "新規作成" : "保存")));
+			Exit();
+		}
+	}
+
+	public class FeedReaderAddIn : AddInBase
+	{
+		public FeedReaderConfiguration Config { get; set; }
+		private Regex _regexHtmlTag = new Regex("<[^>]*>");
+		private Regex _regexLineBreak = new Regex("\r|\n");
+
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			Config = CurrentSession.AddInManager.GetConfig<FeedReaderConfiguration>();
+			Config.Setup(this);
+
+			CurrentSession.AddInsLoadCompleted += (sender, e) =>
+			{
+				CurrentSession.AddInManager.GetAddIn<ConsoleAddIn>().RegisterContext<FeedReaderContext>();
+			};
+		}
+
+		public override void Uninitialize()
+		{
+			Config.Dispose();
+			base.Uninitialize();
+		}
+
+		internal void SaveConfig()
+		{
+			CurrentSession.AddInManager.SaveConfig(Config);
+		}
+
+		internal void OnErrorHandled(object sender, ErrorEventArgs e)
+		{
+			var item = sender as FeedReaderUrlConfiguration;
+			CurrentSession.SendChannelMessage(item.ChannelName, item.SenderNick, e.Exception.Message, true, false, false, true);
+			//CurrentSession.SendChannelMessage(item.ChannelName, item.SenderNick, e.Exception.StackTrace, true, false, false, true);
+		}
+
+		internal void OnPublishDateUpdated(object sender, EventArgs e)
+		{
+			//SaveConfig();
+		}
+
+		internal void OnFeedItemReceived(object sender, FeedReceiveEventArgs e)
+		{
+			var item = sender as FeedReaderUrlConfiguration;
+
+			Func<String, String> conv = str =>
+			{
+				Action<Regex> remove = regex => str = regex.Replace(str ?? String.Empty, String.Empty);
+				if (item.EnableRemoveHtmlTag) remove(_regexHtmlTag);
+				if (item.EnableRemoveLineBreak) remove(_regexLineBreak);
+				return str;
+			};
+
+			StringBuilder content = new StringBuilder(item.ContentFormat);
+			content.Replace("#{feed_title}", conv(e.Document.Title));
+			content.Replace("#{feed_link}", conv(e.Document.Link.ToString()));
+			content.Replace("#{feed_description}", conv(e.Document.Description));
+			content.Replace("#{author}", conv(e.Item.Author));
+			content.Replace("#{link}", conv(e.Item.Link.ToString()));
+			content.Replace("#{title}", conv(e.Item.Title));
+			content.Replace("#{description}", conv(e.Item.Description));
+			content.Replace("#{publish_date}", conv(e.Item.PublishDate.ToString()));
+
+			PrivMsgMessage priv = new PrivMsgMessage();
+			priv.Sender = item.SenderNick;
+			priv.Receiver = item.ChannelName;
+			priv.Content = content.ToString();
+			CurrentSession.Send(priv);
+		}
+
+		internal void RegisterEvent(FeedReaderUrlConfiguration item)
+		{
+			item.ErrorHandled += new EventHandler<ErrorEventArgs>(OnErrorHandled);
+			item.PublishDateUpdated += new EventHandler(OnPublishDateUpdated);
+			item.FeedItemReceived += new EventHandler<FeedReceiveEventArgs>(OnFeedItemReceived);			
+		}
+	}
+}
